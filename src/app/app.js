c363ab0eb9bc4068363287d39500b4c820109ded
Zach Toben <zach.toben@gmail.com>
2019-02-18 09:25:32 -0600

    adding nav bar

--- src/app/app.js
+++ src/app/app.js
@@ -1,7 +1,28 @@
 import React, {Component} from 'react';
+import './app.scss';
 
 export default class App extends Component {
   render() {
-    return <h1>Hello World</h1>;
+    return (
+      <div className="container">
+        <div className="header">
+          <img
+            className="logo"
+            alt="logo"
+            src={require('../../assets/images/logo.png')}
+            width="150"
+          />
+          <div className="link-container">
+            <a href="">About</a>
+            <a href="">Services</a>
+            <a href="">Contact</a>
+            <a href="tel:3192692412" className="phone-number">
+              Call (319) 269-2412
+            </a>
+          </div>
+        </div>
+        <div className="content" />
+      </div>
+    );
   }
 }
