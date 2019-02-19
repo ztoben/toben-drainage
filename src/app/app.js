75440af83123bafb6a143c2061631ae36de0d18c
Zach Toben <zach.toben@gmail.com>
2019-02-18 21:08:49 -0600

    adding about section, more images

--- src/app/app.js
+++ src/app/app.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import {Carousel} from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import './app.scss';
 
 export default class App extends Component {
@@ -13,15 +15,76 @@ export default class App extends Component {
             width="150"
           />
           <div className="link-container">
-            <a href="">About</a>
-            <a href="">Services</a>
-            <a href="">Contact</a>
+            <a href="#about">About</a>
+            <a href="#services">Services</a>
+            <a href="#contact">Contact</a>
             <a href="tel:3192692412" className="phone-number">
               Call (319) 269-2412
             </a>
           </div>
         </div>
-        <div className="content" />
+        <div className="content">
+          <div id="about">
+            <div className="about-text">
+              <h1>About Toben Drainage</h1>
+              <p>
+                We are Jill and Kevin Toben of Toben Drainage, LLC. We started our farm drainage
+                business in 2004 with a Johnson plow and a Steiger tractor.
+              </p>
+              <p>
+                In 2010 we purchased our first commercial Wolfe plow, and we most recently upgraded
+                to a Wolfe PL540. Toben Drainage performs precision farm tiling using GPS
+                technology, as well as commercial and residential drainage for new and existing
+                construction.
+              </p>
+              <p>Toben Drainage also does septic installations and time of transfer inspections.</p>
+            </div>
+            <div className="about-images">
+              <Carousel centerMode infiniteLoop autoPlay>
+                <div>
+                  <img src={require('../../assets/images/new_and_old.jpg')} height={300} />
+                </div>
+                <div>
+                  <img src={require('../../assets/images/stringing.jpg')} height={300} />
+                </div>
+                <div>
+                  <img src={require('../../assets/images/plow_side.jpg')} height={300} />
+                </div>
+                <div>
+                  <img src={require('../../assets/images/backhoe.jpg')} height={300} />
+                </div>
+                <div>
+                  <img src={require('../../assets/images/sunset.jpg')} height={300} />
+                </div>
+              </Carousel>
+            </div>
+          </div>
+          <div id="services">
+            <h1>Services We Provide</h1>
+            <ul>
+              <li>Field drainage</li>
+              <li>Septic installation</li>
+              <li>Time of transfer</li>
+            </ul>
+          </div>
+          <div id="contact">
+            <h1>Contact Us</h1>
+            <form
+              className="email-form"
+              action="mailto:kjtobe@yahoo.com?subject=Message from toben-drainage.com"
+              method="post"
+              encType="text/plain"
+            >
+              <span>Name:</span>
+              <input type="text" name="name" />
+              <span>E-mail:</span>
+              <input type="text" name="mail" />
+              <span>Message:</span>
+              <textarea name="comment" />
+              <input type="submit" value="Send Email" />
+            </form>
+          </div>
+        </div>
       </div>
     );
   }
