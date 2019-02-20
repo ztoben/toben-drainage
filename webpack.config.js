b7e3ce17d34fc6bb255b7cf4effb66b7b7f8f7b9
Zach Toben <zach.toben@gmail.com>
2019-02-20 10:20:10 -0600

    image optimization during bundle, fix mobile header styling

--- webpack.config.js
+++ webpack.config.js
@@ -2,64 +2,91 @@ const webpack = require('webpack');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 
-module.exports = () => {
-  return {
-    entry: ['react-hot-loader/patch', './src/index.js'],
-    module: {
-      rules: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: ['babel-loader'],
-        },
-        {
-          test: /\.(scss|css)$/,
-          use: ['style-loader', 'css-loader', 'sass-loader'],
-        },
-        {
-          test: /\.html$/,
-          use: [
-            {
-              loader: 'html-loader',
-              options: {
-                minimize: true,
-              },
-            },
-          ],
-        },
-        {
-          test: /\.(jpg|png|jpeg)$/,
-          use: {
-            loader: 'url-loader',
+// Image min config
+const ImageminPlugin = require('imagemin-webpack');
+const imageminGifsicle = require('imagemin-gifsicle');
+const imageminMozJpeg = require('imagemin-mozjpeg');
+const imageminPngquant = require('imagemin-pngquant');
+const imageminSvgo = require('imagemin-svgo');
+
+module.exports = {
+  entry: ['react-hot-loader/patch', './src/index.js'],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
             options: {
-              limit: 25000,
+              minimize: true,
             },
           },
+        ],
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 25000,
+          },
         },
-      ],
-    },
-    resolve: {
-      extensions: ['*', '.js', '.jsx'],
-    },
-    output: {
-      path: __dirname + '/dist',
-      publicPath: '/',
-      filename: 'bundle.js',
-    },
-    plugins: [
-      new webpack.HotModuleReplacementPlugin(),
-      new HtmlWebPackPlugin({
-        template: './src/index.html',
-        filename: 'index.html',
-      }),
-      new FaviconsWebpackPlugin('./assets/favicon.png'),
+      },
     ],
-    devServer: {
-      compress: true,
-      contentBase: './dist',
-      hot: true,
-      open: true,
-      port: 9999,
-    },
-  };
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+    new FaviconsWebpackPlugin('./assets/favicon.png'),
+    new ImageminPlugin({
+      bail: false,
+      cache: true,
+      imageminOptions: {
+        plugins: [
+          imageminGifsicle({
+            interlaced: true,
+          }),
+          imageminMozJpeg({
+            progressive: true,
+            quality: 50,
+          }),
+          imageminPngquant({
+            quality: [0.5, 0.6],
+            speed: 10,
+          }),
+          imageminSvgo({
+            removeViewBox: true,
+          }),
+        ],
+      },
+    }),
+  ],
+  devServer: {
+    compress: true,
+    contentBase: './dist',
+    hot: true,
+    open: true,
+    port: 9999,
+  },
 };
