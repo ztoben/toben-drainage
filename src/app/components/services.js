f43363cc73a59dc96bda123fba03909244bf3622
Zach Toben <zach.toben@gmail.com>
2020-01-31 08:51:55 -0600

    some visual updates

--- src/app/components/services.js
+++ src/app/components/services.js
@@ -5,28 +5,30 @@ class Services extends Component {
   render() {
     return (
       <div id="services">
-        <h1>Services We Provide</h1>
-        <h2>Farm Drainage</h2>
-        <ul>
-          <li>Install drainage tile, plow in up to 15" dual wall</li>
-          <li>Tile repairs</li>
-          <li>Jetter service for clearing tile lines</li>
-        </ul>
+        <div className="content-container">
+          <h1>Services We Provide</h1>
+          <h2>Farm Drainage</h2>
+          <ul>
+            <li>Install drainage tile, plow in up to 15" dual wall</li>
+            <li>Tile repairs</li>
+            <li>Jetter service for clearing tile lines</li>
+          </ul>
 
-        <h2>Residential/Commercial Drainage</h2>
-        <ul>
-          <li>Install and repair of foundation drainage</li>
-          <li>Downspout tile</li>
-        </ul>
+          <h2>Residential/Commercial Drainage</h2>
+          <ul>
+            <li>Install and repair of foundation drainage</li>
+            <li>Downspout tile</li>
+          </ul>
 
-        <h2>Septic Systems</h2>
-        <ul>
-          <li>Install new septics, including conventional and alternative systems</li>
-          <li>Repair existing septic systems</li>
-          <li>Perform Time of Transfer inspections</li>
-          <li>Jetter service for clearing clogged lines</li>
-          <li>Sewer camera for diagnostics and locating lines</li>
-        </ul>
+          <h2>Septic Systems</h2>
+          <ul>
+            <li>Install new septics, including conventional and alternative systems</li>
+            <li>Repair existing septic systems</li>
+            <li>Perform Time of Transfer inspections</li>
+            <li>Jetter service for clearing clogged lines</li>
+            <li>Sewer camera for diagnostics and locating lines</li>
+          </ul>
+        </div>
       </div>
     );
   }
